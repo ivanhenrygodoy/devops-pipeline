6ef16cfc376b74a660acb316136e9cb5bb178bf0
Henry <henryivang@gmail.com>
2025-05-19 13:46:14 -0600

    refactor(web-laravel):Mejora manejo de rutas de api

--- laravel-notificacion-logger/routes/web.php
+++ laravel-notificacion-logger/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\EmailController;
+use App\Http\Controllers\logs\LogController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Rutas de la API Laravel-notificacion-logger
+Route::middleware('api')->group(function () {
+    Route::post('/logs', [LogController::class, 'store']);
+    Route::post('/send-email', [EmailController::class, 'sendNotification']);
 });
