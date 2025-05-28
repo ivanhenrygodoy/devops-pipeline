ed6d3db783fc245d266b2a42823bca3e4f300e04
Henry <henryivang@gmail.com>
2025-05-28 13:27:24 -0600

    refactor(index-laravel): ajusta configuraciones para archivo de manejo de peticiones

--- laravel-notificacion-logger/public/index.php
+++ laravel-notificacion-logger/public/index.php
@@ -1,20 +1,49 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\App;
 
-define('LARAVEL_START', microtime(true));
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
+ */
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+$startTime = microtime(true);
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+/**
+ * Error Reporting
+ */
+error_reporting(E_ALL);
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+/**
+ * Define The Application Path
+ *
+ * Get the full path to the application.
+ */
+$appPath = __DIR__.'/../';
 
-$app->handleRequest(Request::capture());
+/**
+ * Require The Composer Autoload File
+ */
+require $appPath.'vendor/autoload.php';
+
+/**
+ * Turn On The Lights
+ */
+$app = require_once $appPath.'bootstrap/app.php';
+
+/**
+ * Run The Application
+ */
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
